ea6c8d5762a54ebd58ad6a5c04de9f6d81e9e1c3
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-05-14 13:14:32 -0700

    Update DNA_File_Seq.py
    Added Start and Stop codon detection

--- DNA_File_Seq.py
+++ DNA_File_Seq.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 
 class DNAStuff(object):
-
+    
+    #Get file
     def DNAfile(self):
         root = tk.Tk()
         messagebox.showinfo("DNA", "Click OK to Choose your File.")
@@ -11,16 +12,31 @@ class DNAStuff(object):
         file_path = filedialog.askopenfilename()
         return file_path
 
+    #Isolate codon triplets
     def triplet_codon(self, line):
         while line:
             data = line.read(3)
             
+    #Shift reading frame 
     def frame_shift_mutation(self):
         #start codon +1 char ahead
         
+    #Start Codon detection
+    def start_codon(self, frame):
+        start_code = ['AUG']
+        if frame in start_code:
+           return True
+    
+    #Stop Codon Detection
+    def stop_codon(self, frame):
+        stop_codes = ['UAA', 'UAG', 'UGA']
+        if frame in stop_codes
+            return True
+    
     def DNA_strand(self, file):
         with open(file) as data:
             for line in data:
                 codon = self.triplet_codon(line)
                 seq = Bio.Seq(line)
                 print(seq)
+    
